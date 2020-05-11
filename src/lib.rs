c8bd63d7808fc8a744d49202a67106793c9fb977
Jeff Davis <jeff@j-davis.com>
2020-05-11 01:46:38 -0700

    Update comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//!
+//! Expose C standard library functions in <setjmp.h> to rust.
+//!
+//! **WARNING: this crate is experimental and even careful use is
+//! likely undefined behavior.**
+//!
+//! See
+//! [setjmp(3)](https://manpages.debian.org/unstable/manpages-dev/setjmp.3.en.html)
+//! and [RFC #2625](https://github.com/rust-lang/rfcs/issues/2625).
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
